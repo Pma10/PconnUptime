8d4fa1d6e07a4b8edea06b9b653400e57cf69bfb
Pma <115062283+Pma10@users.noreply.github.com>
2024-01-05 23:07:31 +0900

    Update lookup.py
    서버 꺼졌을때 옵션 추가

--- Cogs/lookup.py
+++ Cogs/lookup.py
@@ -31,19 +31,27 @@ class Lookup(Cog):
             channel = self.app.get_channel(1192493690699129026)
             if self.messageId is None:
                 embed = discord.Embed(title='Pvpconnect Server Info',color=0xa2cfd6)
-                embed.description = f'\n **{status.motd.parsed[14]}** \n \n **핑** \n > {round(status.latency)}ms \n **버전** \n > {status.version.name}\n **접속자**\n > {player}명 \n **최고 동접** \n > {self.todayMost[0]}명'
+                embed.description = f'\n **{status.motd.parsed[14]}** \n \n **핑** \n > {round(status.latency)}ms \n **버전** \n > {status.version.name}\n **접속자**\n > {player}/{status.players.max}명 \n **최고 동접** \n > {self.todayMost[0]}/{status.players.max}명'
                 embed.timestamp = datetime.utcnow()
                 embed.set_thumbnail(url='https://cdn.discordapp.com/icons/1075028593421340733/94f2cee2abdf7cbb0e7d0330985de477.png?size=4096')
-                message = await channel.send(embed=embed)
+                message = await channel.send(embed=embed)   
                 self.messageId = message.id
             else:
                 message = await channel.fetch_message(self.messageId)
                 embed = message.embeds[0]
                 embed.timestamp = datetime.utcnow()
-                embed.description = f'\n **{status.motd.parsed[14]}** \n \n **핑** \n > {round(status.latency)}ms \n **버전** \n > {status.version.name}\n **접속자**\n > {player}명 \n **최고 동접** \n > {self.todayMost[0]}명'
+                embed.description = f'\n **{status.motd.parsed[14]}** \n \n **핑** \n > {round(status.latency)}ms \n **버전** \n > {status.version.name}\n **접속자**\n > {player}/{status.players.max}명 \n **최고 동접** \n > {self.todayMost[0]}/{status.players.max}명'
                 await message.edit(embed=embed)
         except Exception as e:
-            print(e)
+            if e == TimeoutError:
+                message = await channel.fetch_message(self.messageId)
+                embed = message.embeds[0]
+                embed.timestamp = datetime.utcnow()
+                embed.description = f'서버가 종료되었습니다.'
+                embed.color = 0xFF0000
+                await message.edit(embed=embed)
+            else:
+                print(e)
 
 def setup(app):
     print('[PconnMonitoring] LookUp Start')
