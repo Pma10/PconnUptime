f79092882c11ffd48fd2017ad809b00506737893
Pma <115062283+Pma10@users.noreply.github.com>
2024-01-07 15:51:44 +0900

    Update Mongo.py

--- Command/Mongo.py
+++ Command/Mongo.py
@@ -1,11 +1,14 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.server_api import ServerApi
 
-uri = "dburl"
+uri = "db"
 
 client = AsyncIOMotorClient(uri, server_api=ServerApi('1'))
 
 class MongoDB():
-    async def insertTodayMostUsers(date,users):
+    def insertTodayMostUsers(date,users):
         db = client.pconn
-        await db[date].insert_one({date:users})
+        return db['users'].insert_one({date:users})
+    def insertTodayMostPing(date,ping):
+        db = client.pconn
+        return db['ping'].insert_one({date:ping})
