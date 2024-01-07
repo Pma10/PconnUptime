b4cba6c727c0a76c1d65d561dab4d3b66c2710ea
Pma <115062283+Pma10@users.noreply.github.com>
2024-01-07 15:51:29 +0900

    Update lookup.py

--- Cogs/lookup.py
+++ Cogs/lookup.py
@@ -1,6 +1,6 @@
 import discord
 from Command.Mongo import MongoDB
-from datetime import datetime, timedelta
+from datetime import datetime , timedelta
 from mcstatus import JavaServer
 from discord.ext import commands, tasks
 from discord.ext.commands import Cog
@@ -10,7 +10,8 @@ class Lookup(Cog):
         self.app = app
         self.messageId = 1192609974728282212
         self.pconnAddress = 'pvpconnect.xyz'
-        self.todayMost = [0, datetime.now()]
+        self.todayMostPlayer = [0, datetime.now()]
+        self.todayMostPing = [0, datetime.now()]
         self.checkServerStatus.start()
 
     def cog_unload(self):
@@ -19,40 +20,52 @@ class Lookup(Cog):
     @tasks.loop(seconds=10)
     async def checkServerStatus(self):
         try:
-            server = await JavaServer.async_lookup('pvpconnect.xyz', 25565)
-            status = server.status()            
+            server = await JavaServer.async_lookup(self.pconnAddress, 25565)
+            status = server.status()
             player = status.players.online
-            if player > self.todayMost[0]:
-                self.todayMost[0] = player
-                self.todayMost[1] = datetime.now()
-            if datetime.now().date() != self.todayMost[1].date():
-                await MongoDB.insertTodayMostUsers(datetime.now().date(),self.todayMost[0])
-                self.todayMost[0] = player
-                self.todayMost[1] = datetime.now()
+            ping = int(status.latency)
+            
+            if player > self.todayMostPlayer[0]:
+                self.todayMostPlayer = [player, datetime.now()]
+            
+            if ping > self.todayMostPing[0]:
+                self.todayMostPing = [ping, datetime.now()]
+
+            if datetime.now().date() != self.todayMostPlayer[1].date():
+                MongoDB.insertTodayMostUsers(datetime.now().date() - timedelta(days=1), self.todayMostPlayer)
+                MongoDB.insertTodayMostPing(datetime.now().date() - timedelta(days=1),self.todayMostPing)
+                self.todayMostPlayer = [player, datetime.now()]
+                self.todayMostPing = [ping, datetime.now()]
+            
             channel = self.app.get_channel(1192493690699129026)
-            if self.messageId is None:
-                embed = discord.Embed(title='Pvpconnect Server Info',color=0xa2cfd6)
-                embed.description = f'\n **{status.motd.parsed[14]}** \n \n **핑** \n > {round(status.latency)}ms \n **버전** \n > {status.version.name}\n **접속자**\n > {player}/{status.players.max}명 \n **최고 동접** \n > {self.todayMost[0]}/{status.players.max}명'
-                embed.timestamp = datetime.utcnow()
-                embed.set_thumbnail(url='https://cdn.discordapp.com/icons/1075028593421340733/94f2cee2abdf7cbb0e7d0330985de477.png?size=4096')
-                message = await channel.send(embed=embed)   
+            message = await channel.fetch_message(self.messageId) if self.messageId else None
+
+            embed = discord.Embed(title='Pvpconnect Server Info', color=0xa2cfd6)
+            embed.description = f'\n **{status.motd.parsed[14]}** \n \n **핑** \n > {ping}ms \n **최대 핑** \n > {self.todayMostPing[0]} \n **버전** \n > {status.version.name}\n **접속자**\n > {player}/{status.players.max}명 \n **최고 동접** \n > {self.todayMostPlayer[0]}/{status.players.max}명'
+            embed.timestamp = datetime.utcnow()
+            embed.set_thumbnail(url='https://cdn.discordapp.com/icons/1075028593421340733/94f2cee2abdf7cbb0e7d0330985de477.png?size=4096')
+
+            if message:
+                await message.edit(embed=embed)
+            else:
+                message = await channel.send(embed=embed)
                 self.messageId = message.id
-            else:
-                message = await channel.fetch_message(self.messageId)
-                embed = message.embeds[0]
-                embed.timestamp = datetime.utcnow()
-                embed.description = f'\n **{status.motd.parsed[14]}** \n \n **핑** \n > {round(status.latency)}ms \n **버전** \n > {status.version.name}\n **접속자**\n > {player}/{status.players.max}명 \n **최고 동접** \n > {self.todayMost[0]}/{status.players.max}명'
+
+        except TimeoutError:
+            channel = self.app.get_channel(1192493690699129026)
+            message = await channel.fetch_message(self.messageId) if self.messageId else None
+
+            embed = discord.Embed(title='Pvpconnect Server Info', color=0xFF0000)
+            embed.timestamp = datetime.utcnow()
+            embed.description = '서버가 종료되었습니다.'
+
+            if message:
                 await message.edit(embed=embed)
+            else:
+                message = await channel.send(embed=embed)
+                self.messageId = message.id
         except Exception as e:
-            if e == TimeoutError:
-                message = await channel.fetch_message(self.messageId)
-                embed = message.embeds[0]
-                embed.timestamp = datetime.utcnow()
-                embed.description = f'서버가 종료되었습니다.'
-                embed.color = 0xFF0000
-                await message.edit(embed=embed)
-            else:
-                print(e)
+            print(e)
 
 def setup(app):
     print('[PconnMonitoring] LookUp Start')
