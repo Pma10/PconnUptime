1024d1ae60bcddb7aac7189482fa5664739c234e
Pma <115062283+Pma10@users.noreply.github.com>
2024-01-11 22:15:03 +0900

    Update lookup.py

--- Cogs/lookup.py
+++ Cogs/lookup.py
@@ -1,5 +1,4 @@
-import discord
-from Command.Mongo import MongoDB
+import discord,time
 from datetime import datetime, timedelta
 from mcstatus.server import JavaServer
 from discord.ext import commands, tasks
@@ -12,8 +11,8 @@ class Lookup(Cog):
         self.message_id = 1194152509783167037
         self.channel_id = 1194151956407664711
         self.pconn_address = 'pvpconnect.xyz'
-        self.today_most_player = [0, datetime.now()]
-        self.today_most_ping = [0, datetime.now()]
+        self.today_most_player = [0, 0]
+        self.today_most_ping = [0, 0]
         self.check_server_status.start()
 
     def cog_unload(self):
@@ -24,22 +23,20 @@ class Lookup(Cog):
         try:
             server = await JavaServer.async_lookup(self.pconn_address, 25565)
             status = server.status()
+            nowTimestamp = int(time.mktime(time.localtime()))
             player, ping, max_players = status.players.online, int(status.latency), status.players.max
-            today_date = str(datetime.now().date())
 
             if player > self.today_most_player[0]:
-                self.today_most_player = [player, str(datetime.now())]
-                await MongoDB.update_max('max_players', today_date, player)
+                self.today_most_player = [player, nowTimestamp]
 
             if ping > self.today_most_ping[0]:
-                self.today_most_ping = [ping, str(datetime.now())]
-                await MongoDB.update_max('max_ping', today_date, ping)
+                self.today_most_ping = [ping, nowTimestamp]
 
             channel = self.app.get_channel(self.channel_id)
             message = await channel.fetch_message(self.message_id) if self.message_id else None
 
             embed = discord.Embed(title='Pvpconnect Server Info', color=0xa2cfd6)
-            embed.description = f'\n **{status.motd.parsed[14]}** \n \n **핑** \n > {ping}ms \n **최대 핑** \n > {self.today_most_ping[0]}ms \n **버전** \n > {status.version.name}\n **접속자**\n > {player}/{max_players}명 \n **최고 동접** \n > {self.today_most_player[0]}/{max_players}명'
+            embed.description = f'\n **{status.motd.parsed[14]}** \n \n **핑** \n > {ping}ms \n **최대 핑** \n > {self.today_most_ping[0]}ms <t:{self.today_most_ping[1]}:R> \n **버전** \n > {status.version.name}\n **접속자**\n > {player}/{max_players}명 \n **최고 동접** \n > {self.today_most_player[0]}/{max_players}명 <t:{self.today_most_player[1]}:R>'
             embed.timestamp = datetime.utcnow() + timedelta(hours=9)
             embed.set_thumbnail(url='https://cdn.discordapp.com/icons/1075028593421340733/94f2cee2abdf7cbb0e7d0330985de477.png?size=4096')
 
